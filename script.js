b6a92919c609aa142ee378106b7d09fd7ebb394e
Jashan <jashandeep4329@gmail.com>
2025-07-09 14:16:27 +0530

    upgraded the code a bit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Hello World!")
+
+function getComputerChoice() {
+
+    const num = Math.random();
+    console.log(num);
+
+    if(num < 0.333) return "Rock"
+
+    if(num > 0.666) return "Scissors"
+    else {
+        return "Paper"
+    }
+}
+
+function getUserChoice() {
+
+    let choice = prompt("enter your choice")
+
+    return choice;
+}
+
+const result = getComputerChoice();
+console.log(result);
+
+const userChoice = getUserChoice();
+console.log(userChoice);
