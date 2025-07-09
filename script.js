23c60deb4509913b8dc98843750ae1841e320578
Jashan <jashandeep4329@gmail.com>
2025-07-09 14:38:35 +0530

    added the playRound function:

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("Hello World!")
 
+let computerScore = 0;
+let userScore = 0;
+
+
+
 function getComputerChoice() {
 
     const num = Math.random();
@@ -18,6 +23,31 @@ function getUserChoice() {
     let choice = prompt("enter your choice")
 
     return choice;
+}
+
+
+function playRound(humanChoice, computerChoice) {
+
+     humanChoice = humanChoice.toLowerCase();
+     computerChoice = computerChoice.toLowerCase();
+
+     //if it is a draw
+
+     if(humanChoice === computerChoice){
+        return "it's a tie"
+     }
+
+     if(humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "paper" || 
+        humanChoice === "paper" && computerChoice === "rock"
+     ) {
+        userScore = userScore + 1;
+        return "you won this round"
+     } else {
+        computerScore = computerScore + 1;
+        return "computer won this round"
+     }
+
 }
 
 const result = getComputerChoice();
